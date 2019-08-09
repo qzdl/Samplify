ac50a827115e321594daa5e81642b0aa6ed2cf9f
Samuel Culpepper <samuel@samuelculpepper.com>
2019-08-09 16:03:18 +0100

    formatting to pep8 with yapf

--- sample_finder.py
+++ sample_finder.py
@@ -6,19 +6,27 @@ from spotipy import util
 from tools.whosampled_scrape import *
 import config as cfg
 
+
 def get_playlist_id_from_uri(uri):
     return uri.split(':')[2]
 
+
 def call_api(username, scope='playlist-modify-public'):
-    token = util.prompt_for_user_token(cfg.username, scope, client_id=cfg.client,
-            client_secret=cfg.secret, redirect_uri=cfg.redirect)
+    token = util.prompt_for_user_token(cfg.username,
+                                       scope,
+                                       client_id=cfg.client,
+                                       client_secret=cfg.secret,
+                                       redirect_uri=cfg.redirect)
     return token
 
+
 def make_description(spot_dict):
-    unfound=spot_dict['unfound']
-    rate=spot_dict['rate']
-    summary = 'Samples Not on Spotify: {} \nPercentage of Samples Added: {}%'.format(unfound, round(rate, 3))
+    unfound = spot_dict['unfound']
+    rate = spot_dict['rate']
+    summary = 'Samples Not on Spotify: {} \nPercentage of Samples Added: {}%'.format(
+        unfound, round(rate, 3))
     return summary
+
 
 def read_playlist(uri, sp, link=None):
     # F (FIXME: to pay respects)
@@ -27,8 +35,14 @@ def read_playlist(uri, sp, link=None):
     results = sp.user_playlist(cfg.username, playlist_id)
     for i in results['tracks']['items']:
         artists = [j['name'] for j in i['track']['artists']]
-        og_tracks.append({'artist' : artists, 'track':i['track']['name'].replace('Instrumental', '')})
+        og_tracks.append({
+            'artist':
+            artists,
+            'track':
+            i['track']['name'].replace('Instrumental', '')
+        })
     return og_tracks
+
 
 def get_sample_data(uri, sp):
     loaded_playlist = read_playlist(uri, sp)
@@ -38,30 +52,34 @@ def get_sample_data(uri, sp):
 
 def get_spotify_ids(whosampled_playlist, sp):
     id_list = []
-    unfound_list= []
+    unfound_list = []
     for i in whosampled_playlist:
         sub_list = []
         artist = i['artist'].lower()
-#         print('NEW SAMPLE: {} by {}'.format(i['title'], artist))
+        #         print('NEW SAMPLE: {} by {}'.format(i['title'], artist))
         result = sp.search(i['title'], limit=50)['tracks']['items']
         for j in result:
             if j['artists'][0]['name'].lower() == artist:
                 sub_list.append(j['id'])
                 break
         if sub_list:
-#             print('FOUND ON SPOTIFY')
+            #             print('FOUND ON SPOTIFY')
             id_list.append(sub_list[0])
         else:
-#             print('NO ID FOUND FOR {} by {}'.format(i['title'], artist))
-            unfound_list.append((i['title']+' by '+artist))
-    location_rate=1 - len(unfound_list)/len(whosampled_playlist)
+            #             print('NO ID FOUND FOR {} by {}'.format(i['title'], artist))
+            unfound_list.append((i['title'] + ' by ' + artist))
+    location_rate = 1 - len(unfound_list) / len(whosampled_playlist)
     return {'ids': id_list, 'unfound': unfound_list, 'rate': location_rate}
+
 
 def create_and_populate(username, new_playlist_name, spotify_dict, sp):
     playlist = sp.user_playlist_create(username, new_playlist_name)
-    newest_id = sp.user_playlists(username)['items'][0]['id'] # get ID of playlist just created
-    sp.user_playlist_add_tracks(username, newest_id, spotify_dict['ids'], None) #populate playlist with all samples
+    newest_id = sp.user_playlists(username)['items'][0][
+        'id']  # get ID of playlist just created
+    sp.user_playlist_add_tracks(username, newest_id, spotify_dict['ids'],
+                                None)  #populate playlist with all samples
     pass
+
 
 def get_new_sample_playlist(uri, new_playlist_name, user):
     token = call_api(user, 'playlist-modify-public')
@@ -69,19 +87,25 @@ def get_new_sample_playlist(uri, new_playlist_name, user):
     new_playlist_tracks = get_sample_data(uri, sp2)
     print('\nChecking Spotify for Samples:\n')
     for i in new_playlist_tracks:
-        print(i['title']+' by '+ i['artist'])
+        print(i['title'] + ' by ' + i['artist'])
     spotify_dict = get_spotify_ids(new_playlist_tracks, sp2)
-#     descript = make_description(spotify_dict)
-    new_playlist = create_and_populate(user, new_playlist_name, spotify_dict, sp2)
+    #     descript = make_description(spotify_dict)
+    new_playlist = create_and_populate(user, new_playlist_name, spotify_dict,
+                                       sp2)
     print('\nNew playlist "{}" created!'.format(new_playlist_name))
-#     sp2.user_playlist_change_details(cfg.username, playlist_id, name=new_playlist_name, public=None, collaborative=None,description=description)
+    #     sp2.user_playlist_change_details(cfg.username, playlist_id, name=new_playlist_name, public=None, collaborative=None,description=description)
     pass
 
+
 def main():
-    playlist_uri = input('Please enter the Spotify URI of your playlist. \nThis can be found by clicking "Share" on your playlist and then selecting "Copy Spotify URI":\n>>> ')
-    new_playlist_name = input('Please enter the name of your new sample playlist\n>>> ')
+    playlist_uri = input(
+        'Please enter the Spotify URI of your playlist. \nThis can be found by clicking "Share" on your playlist and then selecting "Copy Spotify URI":\n>>> '
+    )
+    new_playlist_name = input(
+        'Please enter the name of your new sample playlist\n>>> ')
     playlist_id = get_playlist_id_from_uri(playlist_uri)
 
     get_new_sample_playlist(playlist_uri, new_playlist_name, cfg.username)
+
 
 main()
--- tools/whosampled_scrape.py
+++ tools/whosampled_scrape.py
@@ -7,7 +7,11 @@ req = requests.Session()
 adapter = requests.adapters.HTTPAdapter(max_retries=10)
 req.mount('https://', adapter)
 req.mount('http://', adapter)
-req.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"}
+req.headers = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
+}
+
 
 def retrieve_song_link(song_name, artist_name=None):
     query = song_name.replace(' ', '%20')
@@ -20,7 +24,10 @@ def retrieve_song_link(song_name, artist_name=None):
         link = [i.a for i in stuff][0].get('href')
         return link
     # else:
+
+
 #         print('{} not found on whosampled'.format(song_name))
+
 
 def retrieve_samples_v2(song_name, link):
     samples = []
@@ -28,17 +35,53 @@ def retrieve_samples_v2(song_name, link):
     s = req.get(f'https://www.whosampled.com{link}')
     content1 = s.content
     soup = BeautifulSoup(content1, 'html.parser')
-    listed = [i.text for i in soup.findAll('div', attrs={'class':'list bordered-list'})]
+    listed = [
+        i.text
+        for i in soup.findAll('div', attrs={'class': 'list bordered-list'})
+    ]
     if len(listed) == 2:
-        there_in = [i.split('\n') for i in list(filter(None, listed[0].split('\t')))][:-1]
-        there_out = [i.split('\n') for i in list(filter(None, listed[1].split('\t')))][:-1]
+        there_in = [
+            i.split('\n') for i in list(filter(None, listed[0].split('\t')))
+        ][:-1]
+        there_out = [
+            i.split('\n') for i in list(filter(None, listed[1].split('\t')))
+        ][:-1]
         for j in there_out:
-            sampled_by.append({'query':song_name, 'type':j[-7], 'genre':j[-6], 'title':j[-3], 'artist':j[-2].replace('by ', '').split(' (')[0], 'year': j[-2].replace('by ', '').split(' (')[1].replace(')', '')})
+            sampled_by.append({
+                'query':
+                song_name,
+                'type':
+                j[-7],
+                'genre':
+                j[-6],
+                'title':
+                j[-3],
+                'artist':
+                j[-2].replace('by ', '').split(' (')[0],
+                'year':
+                j[-2].replace('by ', '').split(' (')[1].replace(')', '')
+            })
     else:
-        there_in = [i.split('\n') for i in list(filter(None, listed[0].split('\t')))][:-1]
+        there_in = [
+            i.split('\n') for i in list(filter(None, listed[0].split('\t')))
+        ][:-1]
     for i in there_in:
-        samples.append({'query':song_name, 'type':i[-7], 'genre':i[-6], 'title':i[-3], 'artist':i[-2].replace('by ', '').split(' (')[0], 'year': i[-2].replace('by ', '').split(' (')[1].replace(')', '')})
+        samples.append({
+            'query':
+            song_name,
+            'type':
+            i[-7],
+            'genre':
+            i[-6],
+            'title':
+            i[-3],
+            'artist':
+            i[-2].replace('by ', '').split(' (')[0],
+            'year':
+            i[-2].replace('by ', '').split(' (')[1].replace(')', '')
+        })
     return samples, sampled_by
+
 
 def getme_thesamples(song_name, artist_name):
     print(f'getme_thesamples: {song_name}: {artist_name}')
@@ -48,12 +91,13 @@ def getme_thesamples(song_name, artist_name):
     samples, sampled_by = retrieve_samples_v2(song_name, link)
     return samples, sampled_by
 
+
 def get_whosampled_playlist(loaded_playlist):
     samples = []
     new_playlist = []
     print('SPOTIFY PLAYLIST DISCOVERED: \n')
     for i in loaded_playlist:
-        print(i['track']+' by '+i['artist'][0])
+        print(i['track'] + ' by ' + i['artist'][0])
     for i in loaded_playlist:
         samples, sampled_by = getme_thesamples(i['track'], i['artist'][0])
         if samples:
