625f1435f2d66caf674c6e8473faa6166e129e3c
Samuel Culpepper <samuel@samuelculpepper.com>
2020-02-07 14:15:11 +0000

    move to package dir structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="samplify-QZDL", # Replace with your own username
+    version="0.0.1",
+    author="Samuel Culpepper",
+    author_email="library@samuelculpepper.com",
+    description="The sample retrieval engine, powered by WhoSampled",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qzdl/samplify",
+    packages=setuptools.find_packages(),
+    entry_points="""
+     [console_scripts]
+     samplify = samplify
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL 3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
